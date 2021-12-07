9b01a3a2f5624ba1ccd689f16d460554fea93358
Nirmal0210 <95688890+Nirmal0210@users.noreply.github.com>
2021-12-07 16:59:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import React from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import ItemList from './MyComponents/ItemList';
+import Header from './MyComponents/Header';
+import Cart from './MyComponents/Cart';
+export default function App() {
+
+  return (
+    <>
+
+
+      <Header />
+      <ItemList />
+
+
+    </>
+  )
+}
